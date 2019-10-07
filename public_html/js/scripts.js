f9efc65e8d63269d49bbe37fc60f77db681318d9
Wendy Van Craen <wendy.vancraen@gmail.com>
2019-10-07 16:27:54 +0200

    nav codepart done

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -1,6 +1,12 @@
 let mobileMenu = document.getElementById('dropdown-menu-mobile');
 let mainNav = document.getElementById('main-nav');
 
-mobileMenu.addEventListener('click', function(){
-    mainNav.classList.toggle('hidden');
-});
+if(screen.width < 960) {
+    mainNav.classList.add('hidden');
+    mobileMenu.addEventListener('click', function(){
+        mainNav.classList.toggle('hidden');
+    });
+} else {
+    mainNav.classList.remove('hidden');
+}
+
